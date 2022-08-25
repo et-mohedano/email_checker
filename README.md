15bc02475cde29e6b32d4bb4dc8396225ac4d31e
Efrain Mohedano <et.mohedano@gmail.com>
2022-08-24 23:03:53 -0500

    Initial version completed

---
+++ README.md
@@ -0,0 +1,26 @@
+# Email checker
+
+Script to validate a set of emails from xlsx file
+
+## Before install
+
+You need installed:
+
+Python 3.X\
+pandas 1.4.3\
+email-validator 1.2.1
+
+## Before run
+
+You need to edit mails.xlsx and write the email list
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `pip install -r requeriments.txt`
+
+Install python  packages specified for the project
+
+### `python cleaner.py`
+
---
+++ cleaner.py
@@ -0,0 +1,32 @@
+from email_validator import validate_email, EmailNotValidError
+import pandas as pd
+
+def check_email(email):
+    try:
+        v = validate_email(email, check_deliverability=True)
+        email = v["email"]
+        return True
+    except EmailNotValidError:
+        return False
+
+File = pd.ExcelFile('mails.xlsx')
+dataFrame = File.parse('mails')
+validEmails = []
+InvalidEmails = []
+emails_list = dataFrame['mails']
+emails_list = list(set(emails_list))
+print("Process started")
+for emialToSend in emails_list:
+    emialToSend = emialToSend.lower()
+    if check_email(emialToSend):
+        validEmails.append(emialToSend)
+    else:
+        InvalidEmails.append(emialToSend)
+filepath = f'bd_cleaned_valid.xlsx'
+df = pd.DataFrame(zip(validEmails), columns=["Valid emails"])
+df.to_excel(filepath, index=False)
+filepath = f'bd_cleaned_invalid.xlsx'
+df = pd.DataFrame(zip(InvalidEmails), columns=["Invalid emails"])
+df.to_excel(filepath, index=False)
+print(f"Valid emails: {len(validEmails)}, Invalid emails: {len(InvalidEmails)}")
+print("Process finished")
